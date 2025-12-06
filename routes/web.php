9d4d84dbee1b5e9da7abe70aab57a51e1c2a2b0c
Kumandá <62922592+agenciakumanda@users.noreply.github.com>
2025-12-06 09:37:23 -0300

    Add temporary fix-migrate route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/fix-migrate', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate:fresh --seed --force');
+    return "Migraciones reseteadas y seeders ejecutados.";
 });
