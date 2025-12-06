1110a8f939e50ad127e5579bae42ee89eed90cca
Kumandá <62922592+agenciakumanda@users.noreply.github.com>
2025-12-06 10:49:30 -0300

    fix login view path for admin dashboard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ class AuthController extends Controller
 {
     public function showLogin()
     {
-        return view('auth.login');
+        return view('login');
     }
 
     public function login(Request $request)
